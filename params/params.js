f0157188dbbec53b8891d4e3fd6bce2029f2101b
Ayesha Siddika <77181632+Ayesha1116@users.noreply.github.com>
2021-01-09 09:29:01 +0600

    Add files via upload

---
+++ params/params.js
@@ -0,0 +1,10 @@
+
+
+var mongoose = require("mongoose");
+
+const DATABASECONNECTION = "mongodb+srv://ayesha:MlNLdyxuPsDpUE5p@cluster0.oxb3i.mongodb.net/test?retryWrites=true&w=majority"
+
+module.exports = {
+    DATABASECONNECTION
+}
+
